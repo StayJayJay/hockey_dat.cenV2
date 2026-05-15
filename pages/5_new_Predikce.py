6857beac8fbae36f6bc8a62af55cb3f894a83f74
Jakub Jirmann <jirmannjakub@gmail.com>
2026-05-15 12:28:09 +0200

    v0.1.6

--- pages/5_new_Predikce.py
+++ pages/5_new_Predikce.py
@@ -192,16 +192,16 @@ st.write(f"🔥 H2H poslední 3 zápasy: {h2h_form:.2f}")
 # PREDIKCE
 # ==================================================
 
-X_input = pd.DataFrame([{
-    "Home": home,
-    "PP_Diff": pp,
-    "Goalie_rating": goalie,
-    "Team_strength": strength,
-    "quality": quality,
-    "Team_form": team_form,
-    "Opponent_form": opp_form,
-    "H2H_form": h2h_form
-}])
+    X_input = pd.DataFrame([{
+        "Home": home,
+        "PP_Diff": pp,
+        "Goalie_rating": goalie,
+        "Team_strength": strength,
+        "quality": quality,
+        "Team_form": team_form,
+        "Opponent_form": opp_form,
+        "H2H_form": h2h_form
+    }])
 
     prob = model.predict_proba(X_input)[0][1]
 
