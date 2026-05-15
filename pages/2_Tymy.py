26692b3c8898ca9ae12ece4a9c87054b9a37ad87
Jakub Jirmann <jirmannjakub@gmail.com>
2026-05-15 10:35:20 +0200

    v0.0.8

--- pages/2_Tymy.py
+++ pages/2_Tymy.py
@@ -148,3 +148,12 @@ elif diff_xG < 0:
     st.warning(f"{team_b} má vyšší průměrný xG diff o {abs(diff_xG):.2f}")
 else:
     st.info("Týmy mají stejný průměrný rozdíl xG")
+
+diff_PP = ta["avg_PP_Rate"] - tb["avg_PP_Rate"]
+
+diff_PP > 0:
+    st.success(f"{team_a} má vyšší průměr PP o {diff_PP:.2f}")
+elif diff < 0:
+    st.warning(f"{team_b} má vyšší průměr PP o {diff_PP:.2f}")
+else:
+    st.info("Týmy mají stejný průměr využitých PP")
